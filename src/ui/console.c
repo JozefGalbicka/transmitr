7e6eb0c766d44980606a96bfd7c77634de0ce5d6
Jozef Galbička <galb.jozef@gmail.com>
2024-01-03 16:23:04 +0100

    Fix memory leak when loading the file_name from readline() - has be bee free'd after every call

--- src/ui/console.c
+++ src/ui/console.c
@@ -5,10 +5,11 @@
 #include "../utils/strings.h"
 #include <readline/history.h>
 #include <readline/readline.h>
+#include <stdlib.h>
 #include <string.h>
 
 int start_console() {
-    char file_name[50];
+    char *input = NULL;
     _Bool keep_running = 1;
 
     Client cl;
@@ -17,12 +18,13 @@ int start_console() {
 
     printf("Please specify filename to send (write 'exit'/'q' to close the app)\n");
     while (keep_running) {
-        strcpy(file_name, readline("$ "));
-        if (strcmp(file_name, "exit") == 0 || strcmp(file_name, "q") == 0) {
+        input = readline("$ ");
+        if (strcmp(input, "exit") == 0 || strcmp(input, "q") == 0) {
             keep_running = 0;
         } else {
-            client_send_file(localhost_fd, trim(file_name));
+            client_send_file(localhost_fd, trim(input));
         }
+        free(input);
     }
 
     printf("Exiting the app.\n");
