ed5ed540450912a061ab5ad9dc10746cfb2808be
Jozef Galbička <galb.jozef@gmail.com>
2024-01-03 15:53:31 +0100

    Add proper exit option for client-side of the app

--- src/ui/console.c
+++ src/ui/console.c
@@ -8,17 +8,24 @@
 #include <string.h>
 
 int start_console() {
-    printf("Please specify filename to send\n");
     char file_name[50];
+    _Bool keep_running = 1;
 
     Client cl;
     client_init(&cl);
     int localhost_fd = client_connect(&cl, "127.0.0.1");
-    while (1) {
+
+    printf("Please specify filename to send (write 'exit'/'q' to close the app)\n");
+    while (keep_running) {
         strcpy(file_name, readline("$ "));
-        client_send_file(localhost_fd, trim(file_name));
+        if (strcmp(file_name, "exit") == 0 || strcmp(file_name, "q") == 0) {
+            keep_running = 0;
+        } else {
+            client_send_file(localhost_fd, trim(file_name));
+        }
     }
 
+    printf("Exiting the app.\n");
     client_destroy(&cl);
     return 0;
 }
